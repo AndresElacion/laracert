f52cae31564770e1d9cafc111529cc1c15e0caf6
AndresElacion <andreselacioniii@gmail.com>
2024-11-11 17:49:11 +0800

    adjusted route register

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -64,7 +64,7 @@
                                     </p>
                                     <div class="mt-5">
                                         <button class="flex items-center justify-between bg-pink-400 hover:bg-pink-500 text-white font-bold py-2 px-4 rounded-full transition duration-300 transform hover:scale-105">
-                                            <a href="./register.php" class="mr-2 font-bold">
+                                            <a href="{{ route('register') }}" class="mr-2 font-bold">
                                                 Get Started
                                             </a>
                                             <div>
@@ -160,38 +160,6 @@
                             <button class="bg-pink-400 hover:bg-pink-500 text-white font-bold py-2 px-4 rounded-full transition duration-300 transform hover:scale-105">
                                 <a href="{{ route('register') }}">Create Your Account</a>
                             </button>
-                        </div>
-                    </section>
-
-                    <!-- Contact -->
-                    <section id="contact" class="bg-white py-16">
-                        <div class="container mx-auto px-4">
-                            <div class="max-w-2xl mx-auto text-center">
-                                <h1 class="text-4xl font-bold text-gray-800 mb-6">Get in Touch with Us</h1>
-                                <p class="text-gray-600 mb-12">We'd love to hear from you! Whether you have a question about the certificates, features, pricing, or anything else, our team is ready to assist you.</p>
-                            </div>
-
-                            <div class="flex justify-center items-center">
-                                <!-- Contact Form -->
-                                <div class="bg-white  border rounded-lg shadow-lg p-8 md:w-1/2">
-                                    <h2 class="text-2xl font-semibold text-gray-700 mb-6">Send Us a Message</h2>
-                                    <form action="#" method="POST">
-                                        <div class="mb-4">
-                                            <label for="name" class="block text-sm text-gray-600">Full Name</label>
-                                            <input type="text" id="name" name="name" required class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500">
-                                        </div>
-                                        <div class="mb-4">
-                                            <label for="email" class="block text-sm text-gray-600">Email Address</label>
-                                            <input type="email" id="email" name="email" required class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500">
-                                        </div>
-                                        <div class="mb-4">
-                                            <label for="message" class="block text-sm text-gray-600">Message</label>
-                                            <textarea id="message" name="message" rows="5" required class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"></textarea>
-                                        </div>
-                                        <button type="submit" class="w-full bg-blue-500 text-white font-semibold py-2 px-4 rounded-lg hover:bg-blue-600 transition duration-300">Send Message</button>
-                                    </form>
-                                </div>
-                            </div>
                         </div>
                     </section>
 
